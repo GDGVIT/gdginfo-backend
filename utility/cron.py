3af791a566d00ebd15f09a6636b571e2e7708ec9
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-01-24 07:24:58 +0530

    removed redundant func call for cache_analysis

--- utility/cron.py
+++ utility/cron.py
@@ -9,7 +9,6 @@ from utility import utility
 
 
 def cache_job(token, org, redis):
-    utility.cache_analysis(token, org, redis)
     schedule.every().day.at("10:30").do(utility.cache_response, token, org, redis)
     schedule.every().day.at("07:21").do(utility.cache_analysis, token, org, redis)
     while True:
